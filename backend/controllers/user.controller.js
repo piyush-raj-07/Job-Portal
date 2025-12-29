c4277703cb96aee6849a7e22ef7a7fae124f40f2
piyush-raj-07 <piyushraj07092005@gmail.com>
2025-12-30 01:37:42 +0530

    changes many

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -111,7 +111,7 @@ export const login = async (req, res) => {
 
         return res
             .status(200)
-            .cookie("token", token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true, sameSite: "none" })
+            .cookie("token", token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true,secure : true , sameSite: "none" })
             .json({
                 message: `Welcome back, ${user.fullname}.`,
                 user,
